3ff3a9134a9188932a547a6d005e2c92df05be79
Your Name <your_email@example.com>
2026-08-16 16:45:56 +0300

    sql.py

--- sqlite.py
+++ sqlite.py
@@ -18,10 +18,10 @@ cursor.execute(table_info)
 ## insert some more records
 
 cursor.execute('''Insert Into STUDENT values('Emad' , 'Data Science' , 'A' , 90)''')
-cursor.execute('''Insert Into STUDENT values('Milad' , 'Machine Learning' , 'B' , '100')''')
-cursor.execute('''Insert Into STUDENT values('Mariam' , 'Data Science' , 'A' , '85')''')
-cursor.execute('''Insert Into STUDENT values('Mohammed' , 'DEVOPS' , 'A' ,'50')''')
-cursor.execute('''Insert Into STUDENT values('Mohra' , 'DEVOPS' , 'A' , '35')''')
+cursor.execute('''Insert Into STUDENT values('Milad' , 'Machine Learning' , 'B' , 100)''')
+cursor.execute('''Insert Into STUDENT values('Mariam' , 'Data Science' , 'A' , 85)''')
+cursor.execute('''Insert Into STUDENT values('Mohammed' , 'DEVOPS' , 'A' ,50)''')
+cursor.execute('''Insert Into STUDENT values('Mohra' , 'DEVOPS' , 'A' , 35)''')
 ## Display inserted records
 print("The inserted records are")
 data = cursor.execute('''Select * from STUDENT''')
